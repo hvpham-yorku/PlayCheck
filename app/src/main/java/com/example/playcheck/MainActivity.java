5db7fa0394d9e329e6cc93096b59dd5756693505
khalid060 <khalidudu004@gmail.com>
2026-02-20 13:48:26 -0500

    SCRUM-8: added temporary navigation button for game details.

--- app/src/main/java/com/example/playcheck/MainActivity.java
+++ app/src/main/java/com/example/playcheck/MainActivity.java
@@ -8,7 +8,6 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -27,6 +26,14 @@ public class MainActivity extends AppCompatActivity {
         button = findViewById(R.id.logout);
         textView = findViewById(R.id.user_details);
         user = auth.getCurrentUser();
+
+        Button openDetails = findViewById(R.id.openGameDetails);
+
+        openDetails.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, GameDetailsActivity.class);
+            startActivity(intent);
+        });
+
         if (user == null){
             Intent intent = new Intent(getApplicationContext(), Login.class);
             startActivity(intent);
