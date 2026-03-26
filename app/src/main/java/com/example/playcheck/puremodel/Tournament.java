1c41160303eaff05b3ae74a47a841184082d6a21
Marwan Al-Jaufy <marwan.aljaufy@gmail.com>
2026-03-26 10:23:43 -0400

    Refactor Tournament model and add SportType interface
    - Create `SportType` interface in `com.example.playcheck.puremodel`
    - Update `Tournament` class to use a single `Organizer` instead of a list
    - Change `sportType` field in `Tournament` from `String` to the `SportType` interface
    - Implement public getters and setters for all fields in `Tournament.java`
    - Remove redundant XML declaration from `.idea/misc.xml`

--- app/src/main/java/com/example/playcheck/puremodel/Tournament.java
+++ app/src/main/java/com/example/playcheck/puremodel/Tournament.java
@@ -11,7 +11,7 @@ public class Tournament extends Event{
     private List<Game> games;
     private List<Team> teams;
     private List<Referee> referees;
-    private List<Organizer> organizers;
+    private Organizer organizer;
 
     private LocalDate startDate;
     private LocalDate endDate;
@@ -19,7 +19,7 @@ public class Tournament extends Event{
     private String location;
 
     private String tournamentFormat;
-    private String sportType;
+    private SportType sportType;
 
     private int numberOfTeams;
     private int numberOfGames;
@@ -29,6 +29,104 @@ public class Tournament extends Event{
     Tournament(){
 
     }
+
+    public int getTournamentID() {
+        return tournamentID;
+    }
+
+    public String getTournamentName() {
+
+        return tournamentName;
+    }
+
+    public List<Game> getGames() {
+        return games;
+    }
+
+    public List<Team> getTeams() {
+        return teams;
+    }
+
+    public List<Referee> getReferees() {
+        return referees;
+    }
+
+    public Organizer getOrganizer() {
+        return this.organizer;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+    public String getTournamentFormat() {
+        return tournamentFormat;
+    }
+    public SportType getSportType() {
+        return sportType;
+    }
+    public int getNumberOfTeams() {
+        return numberOfTeams;
+    }
+    public int getNumberOfGames() {
+        return numberOfGames;
+    }
+    public String getTournamentDescription() {
+        return tournamentDescription;
+    }
+
+    public void setTournamentID(int tournamentID) {
+        this.tournamentID = tournamentID;
+    }
+    public void setTournamentName(String tournamentName) {
+        this.tournamentName = tournamentName;
+    }
+    public void setGames(List<Game> games) {
+        this.games = games;
+    }
+    public void setTeams(List<Team> teams) {
+        this.teams = teams;
+    }
+    public void setReferees(List<Referee> referees) {
+        this.referees = referees;
+    }
+    public void setOrganizers(Organizer organizer) {
+        this.organizer = organizer;
+    }
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    public void setTournamentFormat(String tournamentFormat) {
+        this.tournamentFormat = tournamentFormat;
+    }
+    public void setSportType(SportType sportType) {
+        this.sportType = sportType;
+    }
+    public void setNumberOfTeams(int numberOfTeams) {
+        this.numberOfTeams = numberOfTeams;
+    }
+    public void setNumberOfGames(int numberOfGames) {
+        this.numberOfGames = numberOfGames;
+    }
+    public void setTournamentDescription(String tournamentDescription) {
+        this.tournamentDescription = tournamentDescription;
+    }
+
+
+
 
 
 
